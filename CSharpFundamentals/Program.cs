4326b46aeb9d0f9ffce038733ca54df1aa0013c4
Carl <Carl@LAPTOP-ENVY-I76>
2018-04-17 07:54:47 +0200

    new List

--- CSharpFundamentals/Program.cs
+++ CSharpFundamentals/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpFundamentals
 {
@@ -10,6 +7,15 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6, 7 });
+
+            foreach (var number in numbers)
+                Console.WriteLine(number);
+            {
+
+            }
         }
     }
 }
