9cdc0aaec54801749252979e044a91e2aeece05a
Carl <Carl@LAPTOP-ENVY-I76>
2018-05-02 07:35:18 +0200

    ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex1
+{
+    class Program
+    {
+        public static bool stijgend(int a, int b)
+        {
+            if (a < b)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please give me input: a few numbers seperated by hyphen (-)");
+            var inp_numbers_hyphen = Console.ReadLine().Trim();
+            bool stijgen, dalen = false;
+            Console.WriteLine("you typed:" + inp_numbers_hyphen);
+            var getallen = inp_numbers_hyphen.Split('-');
+            var reeks=new List<bool>();
+            foreach (var getal in getallen)
+            {
+                Console.WriteLine(getal);
+
+            }
+            for (int i = 0; i < getallen.Length - 1; i++)
+            {
+                if (stijgend(int.Parse(getallen[i]), int.Parse(getallen[i + 1])))
+                {
+                    stijgen = true;
+                    reeks.Add(stijgen);
+
+                }
+                else
+                {
+                    dalen = true;
+                    stijgen = false;
+                }
+                reeks.Add(stijgen);
+                Console.WriteLine("stijgend=" + stijgen + " : " + dalen);
+                stijgen = false;
+                dalen = false;
+            }
+            Console.WriteLine("Tweede getal=" + getallen[1]);
+            if (reeks.Contains(true) && reeks.Contains(false))
+            {
+                Console.WriteLine("ai ai");
+            }
+            else
+            {
+                Console.WriteLine("Prima");
+            }
+        }
+    }
+}
---
+++ Summaryze/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Summaryze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence =
+                "This is going to be an extremely long, really, really extremeley long sentence with lots and lots of words, tokens, numbers etc.";
+            Console.WriteLine(SummarizeText(sentence));
+            Console.WriteLine(SummarizeText(sentence, 40));
+            Console.WriteLine(SummarizeText(sentence, 10));
+
+        }
+
+        static string SummarizeText(string text, int maxL = 25)
+        {
+            //var maxL = 20;
+            if (text.Length < maxL)
+                //Console.WriteLine(text);
+                return text;
+            else
+            {
+                var words = text.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
+
+                foreach (var word in words)
+                {
+                    summaryWords.Add(word);
+                    totalCharacters += word.Length + 1;
+                    if (totalCharacters > maxL)
+                        break;
+                    ;
+                }
+                //var summary = string.Join(" ", summaryWords) + " ...";
+                // Console.WriteLine(summary);
+                return string.Join(" ", summaryWords) + " ...";
+
+            }
+        }
+    }
+}
